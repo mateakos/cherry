5d2885e275ee65f028806ccc5e9247f02f4687b1
Ákos Máté <amate@geomant.com>
2020-03-17 22:23:26 +0100

    Allow CORS calls

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ else
 
 var port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 //Send message for Default URL
 app.get('/', (req,res) => res.send('Hello World with Express'));
 
